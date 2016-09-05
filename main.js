7d984ef545559c3359bed5abc1166f3f70500ccb
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-09-04 19:57:41 -0500

    Here is some more code

--- main.js
+++ main.js
@@ -198,6 +198,7 @@
       /* The slice() method extracts
       parts of a string and returns
       the extracted parts in a new string. */
+      //in this case the slice starts at 19 and ends at 23.
       document.write(randStr.slice(19, 23) + "<br/>");
 
       //Return everything after the start index
@@ -209,4 +210,31 @@
       character at the specified position
       and returns the specified
       number of characters. */
+      //in this case the substr() function
+      // eturn from 19 up till 4 characters after
       document.write(randStr.substr(19, 4) + "<br/>");
+
+      //Replace a string with the .replace() function.
+      document.write(randStr.replace("and on", "forever") + "<br/>"); //"forever" replaces "and on" in the randStr variable.
+
+      //Get chracter at an index with the .charAt() function.
+      /* In this case .chart(2) returns
+      the 2nd character of the string: l */
+      document.write("At Index 2 : ", randStr.charAt(2) + "<br/>");
+      /* In this case .chart(4) returns
+      the 2nd character of the string: n */
+      document.write("At Index 2 : ", randStr.charAt(4) + "<br/>");
+      /* In this case .chart(6) returns
+      the 2nd character of the string: " " *///a blank space
+      document.write("At Index 2 : ", randStr.charAt(6) + "<br/>");
+      /* In this case .chart(10) returns
+      the 2nd character of the string: i */
+      document.write("At Index 2 : ", randStr.charAt(10) + "<br/>");
+      /* In this case .chart(19) returns
+      the 2nd character of the string: g */
+      document.write("At Index 2 : ", randStr.charAt(19) + "<br/>");
+
+      //Split a string into an array with the .split() function.
+      var randStrArray = randStr.split(" ");
+
+      // Trim white space with .trim().
