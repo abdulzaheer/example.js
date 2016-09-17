09c622b610d037a524aac027b75e1164e0c77f99
Jesus Hilario Hernandez <jesushilariohernandez@gmail.com>
2016-09-17 14:45:51 -0500

    added a few blank lines for neatness.

--- formvalidation.js
+++ formvalidation.js
@@ -36,25 +36,25 @@ function isTheFieldEmpty(inputField, helpId) {
   return editNodeText(/^[A-Za-z\.\' \-]{1,15}\s?[A-Za-z\.\' \-]{1,15}\s?[A-Za-z\.\' \-]{1,15}/, inputField.value, helpId, "Please enter a valid name.");
 }
 
+
 // inputField.value – Value typed in the html text box
 function isAddressOk(inputField, helpId) {
-
   return editNodeText(/^[A-Za-z0-9\.\' \-]{5,30}$/, inputField.value, helpId, "Enter a Street (Ex.1234 Main St.)");
 }
 
-function isStateOk(inputField, helpId) {
 
+function isStateOk(inputField, helpId) {
   return editNodeText(/^A[LKSZRAEP]|C[AOT]|D[EC]|F[LM]|G[AU]|HI|I[ADLN]|K[SY]|LA|M[ADEHINOPST]|N[CDEHJMVY]|O[HKR]|P[ARW]|RI|S[CD]|T[NX]|UT|V[AIT]|W[AIVY]$/, inputField.value, helpId, "Enter a State Code in Uppercase (Ex.NY, PA, CA)");
 }
 
-function isPhoneOk(inputField, helpId) {
 
+function isPhoneOk(inputField, helpId) {
   return editNodeText(/^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$/, inputField.value, helpId, "Enter a Phone Number (Ex.412-828-3000)");
 
 }
 
-function isEmailOk(inputField, helpId) {
 
+function isEmailOk(inputField, helpId) {
   return editNodeText(/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/, inputField.value, helpId, "Enter an Email (Ex. derekbanas@newthinktank.com)");
 
 }
